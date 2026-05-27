f14d3b3330688b13ab45b30de8f156b20c7edb11
progenor <nero6126@gmail.com>
2026-05-27 11:04:40 +0300

    4.825 0.001 60 5

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ int currentPwmL = 0;
 int currentPwmR = 0;
 
 // Core Tuning Variables (Modified by Menu/Serial)
-double Kp = 1.525;
+double Kp = 4.825;
 double Kd = 0.001;
 double BaseSpeed = 60.0;
 double MaxTurnSpeed = 5.0;
